6e9086daa8dcb7acb460863d5390539f58b70b1d
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-02-11 12:49:18 +0100

    initialization

--- lib/main.dart
+++ lib/main.dart
@@ -1,82 +1,35 @@
 import 'package:flutter/material.dart';
-import './quiz.dart';
-import './result.dart';
 
-// void main() {
-//   runApp(MyApp());
-// }
 void main() => runApp(MyApp());
 
-class MyApp extends StatefulWidget {
-  @override
-  _MyAppState createState() => _MyAppState();
-}
-
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'What\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 10},
-        {'text': 'Red', 'score': 5},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 1},
-      ]
-    },
-    {
-      'questionText': 'What\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 5},
-        {'text': 'Snake', 'score': 1},
-        {'text': 'Elephant', 'score': 10},
-        {'text': 'Lion', 'score': 20},
-      ]
-    },
-    {
-      'questionText': 'What\'s your favorite instructor?',
-      'answers': [
-        {'text': 'Max', 'score': 5},
-        {'text': 'Marc', 'score': 1},
-        {'text': 'Oto', 'score': 1},
-        {'text': 'Max', 'score': 2},
-      ]
-    },
-  ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
-
-  void _resetQuiz() {
-    setState(() {
-      _questionIndex = 0;
-      _totalScore = 0;
-    });
-  }
-
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      _questionIndex++;
-    });
-    print(_questionIndex);
-    if (_questionIndex < _questions.length) {
-      print('We have more questions');
-    }
-  }
-
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                questions: _questions,
-                answerQuestion: _answerQuestion,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore, _resetQuiz),
+      title: 'DeliMeals',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+    @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('DeliMeals'),
+      ),
+      body: Center(
+        child: Text('Navigation Time!'),
       ),
     );
   }
