a271b2d7698572c3efc1fad89c7943b216b9191d
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-03-05 13:14:24 +0100

    Popping pages & passing data back

--- lib/screens/filters_screen.dart
+++ lib/screens/filters_screen.dart
@@ -1,11 +1,20 @@
 import 'package:flutter/material.dart';
+import '../widgets/main_drawer.dart';
 
 class FiltersScreen extends StatelessWidget {
   static const routeName = '/filters';
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Filters!'),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Your Filters'),
+      ),
+      //when we don't want to go back, we can comment this
+      //in the situation of, after login disapear the option to go back
+      drawer: MainDrawer(),
+      body: Center(
+        child: Text('The Favorites'),
+      ),
     );
   }
 }
