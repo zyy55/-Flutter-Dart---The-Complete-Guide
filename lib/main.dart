a815279d7cccf4c00e3d463eecd8e8a8bcba843f
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-03-17 13:38:03 +0100

    Adding a "Manage Products" page

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart'; // remove t
 import './providers/cart.dart'; // he comment for this line
 import './providers/orders.dart';
 import './screens/orders_screen.dart';
+import './screens/user_products_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -54,6 +55,7 @@ class MyApp extends StatelessWidget {
           ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
           CartScreen.routeName: (ctx) => CartScreen(),
           OrdersScreen.routeName: (ctx) => OrdersScreen(),
+          UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
         },
       ),
     );
