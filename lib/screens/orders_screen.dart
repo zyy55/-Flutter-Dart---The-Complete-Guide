4e483ebccd45dab2dc1cda7cde2a95a45e439378
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-03-22 11:31:43 +0100

    another way to load content

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -13,19 +13,23 @@ class OrdersScreen extends StatefulWidget {
 
 class _OrdersScreenState extends State<OrdersScreen> {
   var _isLoading = false;
+
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+    // Future.delayed(Duration.zero).then((_) async {
+
+    _isLoading = true;
+
+    // if we use liste: false, we don't need to use Future.delayed
+    Provider.of<Orders>(context, listen: false).fetchAndSetOrders().then((_) {
       setState(() {
         _isLoading = false;
       });
     });
+
     super.initState();
   }
+
   @override
   Widget build(BuildContext context) {
     final orderData = Provider.of<Orders>(context);
@@ -34,12 +38,16 @@ class _OrdersScreenState extends State<OrdersScreen> {
         title: Text('Your Orders'),
       ),
       drawer: AppDrawer(),
-      body: _isLoading ? Center(child: CircularProgressIndicator(),) : ListView.builder(
-        itemCount: orderData.orders.length,
-        itemBuilder: (ctx, i) => OrderItem(
-          orderData.orders[i],
-        ),
-      ),
+      body: _isLoading
+          ? Center(
+              child: CircularProgressIndicator(),
+            )
+          : ListView.builder(
+              itemCount: orderData.orders.length,
+              itemBuilder: (ctx, i) => OrderItem(
+                orderData.orders[i],
+              ),
+            ),
     );
   }
 }
