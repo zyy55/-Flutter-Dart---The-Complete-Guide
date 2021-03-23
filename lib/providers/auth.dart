7182de4e869928b80583b5a8d710b399a1f1d3e0
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-03-23 12:22:22 +0100

    aDDING A lOGOUT fUNCTIONALITY

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -66,4 +66,11 @@ class Auth with ChangeNotifier {
   Future<void> login(String email, String password) async {
     return _authrnticate(email, password, 'signInWithPassword');
   }
+
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    notifyListeners();
+  }
 }
