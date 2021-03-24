7c5e5967d37ab2381c500a9f1148b3694130921e
Yuanyu Zhou <yuanyu.zhou@servicetonic.com>
2021-03-24 11:00:07 +0100

    Adding a "Hero" Transition

--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -31,10 +31,13 @@ class ProductItem extends StatelessWidget {
               arguments: product.id,
             );
           },
-          child: FadeInImage(
-            placeholder: AssetImage('assets/images/product-placeholder'),
-            image: NetworkImage(product.imageUrl),
-            fit: BoxFit.cover,
+          child: Hero(
+            tag: product.id,
+            child: FadeInImage(
+              placeholder: AssetImage('assets/images/product-placeholder'),
+              image: NetworkImage(product.imageUrl),
+              fit: BoxFit.cover,
+            ),
           ),
         ),
         footer: GridTileBar(
